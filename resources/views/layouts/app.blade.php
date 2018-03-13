2834c5d58424260b0627cdd3a333dfd8bb90c2a6
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-13 22:49:07 +0800

    Add text-white to cart span

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('cart.index') }}">Cart 
                                 @if(Cart::instance('default')->count() > 0)
-                                <span class="badge badge-warning">{{ Cart::instance('default')->count() }}</span>                            
+                                <span class="badge badge-warning text-white">{{ Cart::instance('default')->count() }}</span>                            
                                 @endif
                             </a>
                         </li>
@@ -53,7 +53,7 @@
                         <li class="nav-item">
                         <a class="nav-link" href="{{ route('cart.index') }}">Cart 
                             @if(Cart::instance('default')->count() > 0)
-                            <span class="badge badge-warning">{{ Cart::instance('default')->count() }}</span>                            
+                            <span class="badge badge-warning text-white">{{ Cart::instance('default')->count() }}</span>                            
                             @endif
                         </a>
                         </li>
