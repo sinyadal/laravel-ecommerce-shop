16ba2da01786fab7443bb13dfa6c4463fbc17816
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-13 10:02:17 +0800

    Configure route for home-page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/here', '/there', 301);
+
+// Route::get('/', function () {
+//     return view('home-page');
+// });
+
+Route::get('/', 'HomePageController@index')->name('homepage.index');
 
 Auth::routes();
 
