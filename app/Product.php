8f9d1338fafa6e50e6ab78322d42c61f784ebe85
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-13 10:01:50 +0800

    Add money currency

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // Price currency
+    public function presentPrice()
+    {
+        return money_format('$%i', $this->price / 100);
+    }
 }
