09c6848dc63d540fd5f69a98a77e0d50e58e8d42
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-15 13:05:13 +0800

    Fix relay

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function products()
+    public function categories()
     {
         return $this->belongsToMany('App\Category');
     }
