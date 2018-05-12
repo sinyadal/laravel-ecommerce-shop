6aa0f4118e36cc3b6b3fc4c38bff39fe19790a02
sinyadal <v0xe7y2cl@protonmail.com>
2018-05-12 15:11:31 +0800

    Update

--- app/Http/Controllers/ShopPageController.php
+++ app/Http/Controllers/ShopPageController.php
@@ -12,24 +12,26 @@ class ShopPageController extends Controller
     {
         $categories = Category::all();
         // Category filtering
-        if (request()->category) { // If request has $category->slug passed, filter the query..
+        if (request()->category) { 
+            // If request has $category->slug passed, filter the query..
             $products = Product::with('categories')->whereHas('categories', function ($query) {
                 $query->where('slug', request()->category);
             });
             $category_name = optional($categories->where('slug', request()->category)->first())->name;
-        } else { // Just normal behavior
+        } else { 
+            // Just normal behavior
             $products = Product::where('featured', true);
             $category_name = 'Featured';
         }
         // Price sorting
-        if (request()->price_sort == 'low_high') { // If request has $category->slug passed, filter the query..
+        if (request()->price_sort == 'low_high') { 
+            // If request has $category->slug passed, filter the query..
             $products = $products->orderBy('price')->paginate(6);
         } elseif (request()->price_sort == 'high_low') {
             $products = $products->orderBy('price', 'desc')->paginate(6);
         } else {
             $products = $products->paginate(6);
         }
-        
         return view('shop', compact('products', 'categories', 'category_name'));
     }
     public function show($slug)
